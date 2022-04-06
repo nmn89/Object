d8cca5b8217fecc026f0c0e4b080bd8dfed89aa7
nmn89 <98sharmanaman@gmail.com>
2022-04-06 18:49:31 +0530

    Servlet

---
+++ Data.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class Data extends HttpServlet {
+ 
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      String title = "Using GET Method to Read Form Data";
+      String docType =
+         "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+      out.println(docType +
+         "<html>\n" +
+            "<head><title>" + title + "</title></head>\n" +
+            "<body bgcolor = \"#f0f0f0\">\n" +
+               "<h1 align = \"center\">" + title + "</h1>\n" +
+               "<ul>\n" +
+                  "  <li><b>First Name</b>: "
+                  + request.getParameter("first_name") + "\n" +
+                  "  <li><b>Last Name</b>: "
+                  + request.getParameter("last_name") + "\n" +
+               "</ul>\n" +
+            "</body>" +
+         "</html>"
+      );
+   }
+}
